11aaf70b138bc48025ac10ca1ae9ef5b8798c19c
Tiago Gouvêa <tiago@tiagogouvea.com.br>
2020-05-30 10:03:58 -0300

    Updated script

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const dpl = (params) => {
         return `--${key}='${params[key]}' `
     }).join('');
 
-    const cmd = `docker run -v $(pwd):/tmp tiagogouvea/dpl ` + paramsString;
+    const cmd = `docker run -v $(pwd):/tmp tiagogouvea/dpl ` + paramsString+  ' --skip-cleanup';
     console.log("cmd", cmd);
 
     const r = execSync(cmd);
